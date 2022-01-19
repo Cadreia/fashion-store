2424fd4e37140be729b61036e9562ee45622e902
Audrey Ndum <audreyndum@gmail.com>
2022-01-20 00:15:43 +0100

    preprocess collections data, convert to hash table format

--- src/pages/shop/shoppage.component.jsx
+++ src/pages/shop/shoppage.component.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import { collection, onSnapshot } from "firebase/firestore";
+import React, { Component } from "react";
 import { Outlet } from "react-router-dom";
+import { db, transformCollectionsSnapshotToMap } from "../../firebase/firebase.utils";
 
-const ShopPage = () => {
-  return (
-    <div className="shop-page">
-      <Outlet />
-    </div>
-  );
-};
+class ShopPage extends Component {
+  unsubscribeFromSnapshot = null;
+  componentDidMount() {
+    const collectionRef = collection(db, "collections");
+
+    // retrieve data onComponentMount or when collectionRef changes
+    onSnapshot(collectionRef, async (collections) => {
+      transformCollectionsSnapshotToMap(collections)
+    });
+  }
+  render() {
+    return (
+      <div className="shop-page">
+        <Outlet />
+      </div>
+    );
+  }
+}
 
 export default ShopPage;
