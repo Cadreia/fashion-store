f91db316cce580ad401f5b20d69123a34084d9b3
Audrey Ndum <audreyndum@gmail.com>
2022-01-20 00:36:59 +0100

    updated redux state with processed shop collections data from firestore

--- src/pages/shop/shoppage.component.jsx
+++ src/pages/shop/shoppage.component.jsx
@@ -1,17 +1,28 @@
 import { collection, onSnapshot } from "firebase/firestore";
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { Outlet } from "react-router-dom";
-import { db, transformCollectionsSnapshotToMap } from "../../firebase/firebase.utils";
+import {
+  db,
+  transformCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
+import { updateCollections } from "../../redux/shop/shop.actions";
 
 class ShopPage extends Component {
   unsubscribeFromSnapshot = null;
+
   componentDidMount() {
+    const { updateCollections } = this.props;
     const collectionRef = collection(db, "collections");
 
     // retrieve data onComponentMount or when collectionRef changes
-    onSnapshot(collectionRef, async (collections) => {
-      transformCollectionsSnapshotToMap(collections)
-    });
+    this.unsubscribeFromSnapshot = onSnapshot(
+      collectionRef,
+      async (collections) => {
+        const collectionsMap = transformCollectionsSnapshotToMap(collections);
+        updateCollections(collectionsMap);
+      }
+    );
   }
   render() {
     return (
@@ -22,4 +33,9 @@ class ShopPage extends Component {
   }
 }
 
-export default ShopPage;
+const mapDispatchToProps = (dispatch) => ({
+  updateCollections: (collectionsMap) =>
+    dispatch(updateCollections(collectionsMap)),
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
