3e54f59ad914f25822a3b0852404d68d5df1b373
Audrey Ndum <audreyndum@gmail.com>
2022-04-04 16:16:52 +0100

    replace component lifecycle methods with useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import HomePage from "./pages/home/homepage.component";
 import { Routes, Route, Navigate } from "react-router-dom";
@@ -14,48 +14,37 @@ import CollectionsOverviewContainer from "./components/collections-overview/coll
 import CollectionPageContainer from "./pages/collection/collection.container";
 import { checkUserSession } from "./redux/user/user.actions";
 
-class App extends Component {
-  unSubscribeFromAuth = null;
-
-  componentDidMount() {
-    const { checkUserSession } = this.props;
+const App = ({ checkUserSession, currentUser }) => {
+  useEffect(() => {
     checkUserSession();
-  }
+  }, [checkUserSession]);
 
-  componentWillUnmount() {
-    this.unSubscribeFromAuth();
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<HomePage />} />
-          <Route path="/shop" element={<ShopPage />}>
-            <Route index element={<CollectionsOverviewContainer />} />
-            <Route
-              exact
-              path=":collectionId"
-              element={<CollectionPageContainer />}
-            />
-          </Route>
-          <Route exact path="/checkout" element={<CheckoutPage />} />
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />}>
+          <Route index element={<CollectionsOverviewContainer />} />
           <Route
             exact
-            path="/auth"
-            element={
-              this.props.currentUser ? <Navigate replace to="/" /> : <Auth />
-            }
+            path=":collectionId"
+            element={<CollectionPageContainer />}
           />
-          {/* <Route exact path="/login" element={<Login/>}/>
+        </Route>
+        <Route exact path="/checkout" element={<CheckoutPage />} />
+        <Route
+          exact
+          path="/auth"
+          element={currentUser ? <Navigate replace to="/" /> : <Auth />}
+        />
+        {/* <Route exact path="/login" element={<Login/>}/>
             <Route exact path="/recovery-password" element={<RecoveryPassword/>}/>
             <Route path="*" element={<NotFound/>}/> */}
-        </Routes>
-      </div>
-    );
-  }
-}
+      </Routes>
+    </div>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
